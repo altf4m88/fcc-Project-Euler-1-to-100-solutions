06dfcf571e4dd2458d6e9c8affd99d052655ac23
fadhilmauladhani <fadhilmauladhani@gmail.com>
2024-07-17 14:21:21 +0700

    add solution 22

---
+++ problem-22-names-scores.js
@@ -0,0 +1,51 @@
+const alphabeticalScore = {
+  'A' : 1,
+  'B' : 2,
+  'C' : 3,
+  'D' : 4,
+  'E' : 5,
+  'F' : 6,
+  'G' : 7,
+  'H' : 8,
+  'I' : 9,
+  'J' : 10,
+  'K' : 11,
+  'L' : 12,
+  'M' : 13,
+  'N' : 14,
+  'O' : 15,
+  'P' : 16,
+  'Q' : 17,
+  'R' : 18,
+  'S' : 19,
+  'T' : 20,
+  'U' : 21,
+  'V' : 22,
+  'W' : 23,
+  'X' : 24,
+  'Y' : 25,
+  'Z' : 26,
+}
+
+function namesScores(arr) {
+  let sortedArr = arr.sort();
+  let totalScore = 0;
+
+  sortedArr.forEach((name, nameIndex) => {
+    let currentNameScoreSum = 0;
+    name.split('').forEach((item) => {
+      currentNameScoreSum += alphabeticalScore[item];
+    })
+
+    totalScore += currentNameScoreSum * (nameIndex + 1)
+  })
+
+  return totalScore;
+}
+
+
+// Only change code above this line
+const test1 = ['THIS', 'IS', 'ONLY', 'A', 'TEST'];
+const test2 = ['I', 'REPEAT', 'THIS', 'IS', 'ONLY', 'A', 'TEST'];
+
+namesScores(test1);
